bc030098ac08444d234b1a1dc5e08f8cf9c8c3b4
CK <1012941302@qq.com>
2021-05-14 14:06:56 +0800

    再把第一次的传过去

--- pages/musicModule/musicModule.vue
+++ pages/musicModule/musicModule.vue
@@ -1,9 +1,22 @@
 <template>
 	<view class="container">
-		<view></view>
-		<view></view>
-		<view></view>
-		<view></view>
+		<view>
+			<swiper indicator-dots="true" circular="true" autoplay="true" interval="2000" duration="1000">
+				<block >
+					<swiper-item>
+						<!-- 这里放轮播图片 -->
+					</swiper-item>
+				</block>
+			</swiper>
+		</view>
+		<view class="playlist-container">
+			<block>
+				<c-playlist ></c-playlist>
+			</block>
+		</view>
+		<view>
+			<!-- 这里是每周推荐 -->
+		</view>
 	</view>
 </template>
 
@@ -11,15 +24,21 @@
 	export default {
 		data() {
 			return {
-				
+
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
 
 <style>
-
+.playlist-container{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin-top: 10rpx;
+  flex-direction: row;
+}
 </style>

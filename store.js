d91a240a749d056f821c5df46296a9b49b898a00
DM <276061317@qq.com>
2021-05-14 13:50:05 +0800

    12

---
+++ store.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+const store = new Vuex.Store({
+	state: {
+		loginStatus: false,
+		user: {}
+	},
+	mutations: {
+		login(state, user) {
+			state.loginStatus = true
+			state.user = user
+			uni.setStorageSync('user', JSON.stringify(user));
+		}
+		
+	}
+})

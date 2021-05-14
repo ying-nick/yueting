1b2858a9ab57a696251671f78b89e0be51d27014
name-xc <1749960239@qq.com>
2021-05-14 16:45:58 +0800

    样式修改

--- pages/search/search.vue
+++ pages/search/search.vue
@@ -1,12 +1,16 @@
 <template>
-	<view>
+	<view class="container">
 		<view class="head">
 			<view class="searchView">
-				<input type="text" value="" placeholder="请输入" maxlength="15"/>
+				<input type="text" value="" placeholder="请输入" maxlength="15"  class="bar"/>
+				<button type="default" size="mini" class="searchBtn">搜索</button>
 			</view>
 		</view>
 		<view class="main">
-			
+
+		</view>
+		<view class="bottom">
+
 		</view>
 	</view>
 </template>
@@ -15,17 +19,44 @@
 	export default {
 		data() {
 			return {
-				
+
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
 
 <style lang="less">
-	.searchView{
-		
+	.container {
+		.head {		
+			.searchView{			
+				    display: flex;
+				    align-items: center;
+				    margin-left: 20rpx;
+				    position: relative;
+
+				.bar{	
+							flex: 1;
+					    height: 68rpx;
+					    font-size: 28rpx;
+					    border-radius: 50px;
+					    background-color: #C0C0C0;
+					    padding-left: 60rpx;
+				}
+				.searchBtn{
+				        background: #d43c33;
+				        color: #fff;
+				        margin-left: 10rpx;
+								margin-right: 10rpx;
+				        font-size: 28rpx;
+				}
+			}
+		}
+
+		.main {}
+
+		.bottom {}
 	}
 </style>

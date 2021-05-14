bcae4ffbf73cd416db7a03f4d2c671d2f36714c5
DM <276061317@qq.com>
2021-05-14 16:06:19 +0800

    121

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -4,7 +4,14 @@
 			<text>悦听音乐</text>
 		</view>
 		<view class="login">
-
+			<view class='logo'>
+				<view class="cicl-1">
+					<view class="cicle-2">
+						<image src="../../static/logo/yinyue.png" class="img"></image>
+					</view>
+				</view>
+			</view>
+			<view class="phone-log"></view>
 		</view>
 	</view>
 </template>
@@ -30,10 +37,9 @@
 <style lang="less" scoped>
 	.contain {
 		height: 100%;
-
+		background-color: #72D0EA;
 
 		.top-bar {
-			background-color: #4CD964;
 			height: 13%;
 			display: flex;
 			justify-content: center;
@@ -43,8 +49,49 @@
 		}
 
 		.login {
-			background-color: #DD524D;
 			height: 87%;
+			display: flex;
+			flex-direction: column;
+
+			.logo {
+				height: 200px;
+				width: 200px;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+
+				.cicl-1 {
+					height: 190px;
+					width: 190px;
+					border: 1px solid #666666;
+					border-radius: 50%;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+
+					.cicle-2 {
+						height: 150px;
+						width: 150px;
+						border: 1px solid #666666;
+						border-radius: 50%;
+						display: flex;
+						justify-content: center;
+						align-items: center;
+
+						.img {
+							width: 110px;
+							height: 110px;
+						}
+					}
+
+				}
+
+			}
+
+			.phone-log {
+				flex: 1;
+				background-color: #666666;
+			}
 		}
 	}
 </style>

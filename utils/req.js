3425daf3358250a2739dfd203f9dbde1813656d5
CK <1012941302@qq.com>
2021-07-21 18:28:58 +0800

    no message

--- utils/req.js
+++ utils/req.js
@@ -1,12 +1,12 @@
-<<<<<<< HEAD
+
 // const baseUrl = "http://localhost:3000";
 const baseUrl = 'https://y-nick.com:3000'
 
-=======
-const baseUrl = "https://y-nick.com:3000";
+
+
 // const baseUrl = 'https://netease-cloud-music-api-lilac-three.vercel.app'
 // const baseUrl = 'https://pl-fe.cn/cloud-music-api'
->>>>>>> cde89b83a50afd606dce54d2ae78f84b9ffc5ca1
+
 export function myRequestGet(url, data) {
 	return new Promise((resolve, reject) => {
 		uni.request({
